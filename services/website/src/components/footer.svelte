928f699d9e054d10bbbd8842180d329a04468cf6
cliff <cliff.wong@ehlacademy.org>
2021-01-09 15:01:30 +0800

    footer

--- services/website/src/components/footer.svelte
+++ services/website/src/components/footer.svelte
@@ -1,38 +1,8 @@
-<script>
-  import A from "./ui-elements/a.svelte";
-</script>
-
-<footer class="bg-gray-900">
-  <div
-    class="container pb-24 w-full px-4 md:px-0 max-w-6xl mx-auto flex items-center
-    py-8">
-    <div class="w-full mx-0 sm:mx-6">
-      <div class="w-full mx-auto flex flex-wrap items-center">
-        <div
-          class="flex w-full md:w-1/2 justify-center md:justify-start text-white
-          font-extrabold">
-          <p class="no-underline">
-            <span class="text-base text-white">據點。句點</span>
-          </p>
-        </div>
-        <div
-          class="flex w-full content-center justify-between md:w-1/2
-          md:justify-end">
-          <ul
-            class="list-reset flex justify-center flex-1 md:flex-none
-            items-center">
-            <li>
-              <A
-                class="inline-block py-2 px-3 text-white no-underline
-                hover:text-gray-200 hover:underline"
-                href="https://www.facebook.com/floatingprojects"
-                target="_blank">
-                Facebook
-              </A>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+<footer class="bg-gray-200 pt-8">
+  <div class="pb-8 text-center">
+    <img src="images/logo.png" alt="logo" class="mx-auto w-16 mb-4">
+    <p class="mb-4"><b>Empower</b> 由一群懷抱夢想與理想，追求不平凡人生的平凡人組成。</p>
+    <p class="font-sm mb-4 text-gray-700">Email: info@empower.hk</p>
+    <p class="text-sm text-gray-700">Empower © Copyright 2018. All Rights Reserved</p>
   </div>
 </footer>
