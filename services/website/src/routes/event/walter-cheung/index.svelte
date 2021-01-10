06771e89904e5ffef8263ddab08f3326612fa43f
empowerhkinfo <empowerhkinfo@gmail.com>
2021-01-10 04:10:23 +0000

    add 2 posts

---
+++ services/website/src/routes/event/walter-cheung/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2021-01-10-walter-cheung/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
