d008af8ec6df6f2bb13015e64ccee9f573bbeaf2
empowerhkinfo <empowerhkinfo@gmail.com>
2021-01-10 05:00:47 +0000

    edit mass

---
+++ services/website/src/routes/event/ng-cheung/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/21-05-22-ng-cheung/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
