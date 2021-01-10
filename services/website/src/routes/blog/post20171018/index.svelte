284522b4a40ce51b44a2203a398b316753dad2ef
empowerhkinfo <empowerhkinfo@gmail.com>
2021-01-10 07:33:53 +0000

    enter post

---
+++ services/website/src/routes/blog/post20171018/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2017-10-18-post20171018/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
