462103cd78f0aaaa06562060b7590d5f5cc72803
empowerhkinfo <empowerhkinfo@gmail.com>
2021-01-11 15:59:24 +0000

    add many posts and events

---
+++ services/website/src/routes/event/hiking1701/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2017-01-21-hiking1701/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
