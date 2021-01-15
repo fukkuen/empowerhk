996d1fd6876a1cea661c0074f10108fbdbdb9130
empowerhkinfo <empowerhkinfo@gmail.com>
2021-01-15 03:46:55 +0000

    add many posts

---
+++ services/website/src/routes/event/dr-luk/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2017-12-16-dr-luk/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
