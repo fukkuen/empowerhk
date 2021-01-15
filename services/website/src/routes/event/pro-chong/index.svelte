a57a4a352ca7d2dedc41c3d931bfb6ef0dece430
empowerhkinfo <empowerhkinfo@gmail.com>
2021-01-15 16:18:36 +0000

    add all guest

---
+++ services/website/src/routes/event/pro-chong/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2020-03-15-pro-chong/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
