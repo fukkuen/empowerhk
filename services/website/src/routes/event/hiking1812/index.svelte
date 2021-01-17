c7cd4a398f2bbdfbdb214ea5bb80db62e7e1dbdb
empowerhkinfo <empowerhkinfo@gmail.com>
2021-01-17 15:23:41 +0000

    added events

---
+++ services/website/src/routes/event/hiking1812/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2018-12-08-hiking1812/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
