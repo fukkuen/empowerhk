881b2d0bc66b00dc2d930fbaa119dd07d91da586
cliff <cliff.wong@ehlacademy.org>
2021-01-17 20:50:28 +0800

    blog list view layout

--- services/website/src/components/blog-post/previews/index.svelte
+++ services/website/src/components/blog-post/previews/index.svelte
@@ -1,5 +1,5 @@
 <script>
-  import A from "../../ui-elements/a.svelte";
+  import IMG from "../../ui-elements/bg-image.svelte";
   import PostAuthor from './post-author.svelte'
 
   export let post;
@@ -14,14 +14,12 @@
     <div class="p-4">
       <PostAuthor {post}/>
     </div>
-    <img
-            src={coverImageBasePath}
-            loading="lazy"
-            class="w-full"
-            alt="Cover picture for a blog post titled {post.metadata.title}" />
+    <div class="bg-gray-500">
+      <IMG src={coverImageBasePath}/>
+    </div>
     <div class="px-4 py-8">
       <div class="mb-4">
-        <p class="text-xs uppercase text-gray-600">{post.metadata.createdAt}</p>
+        <p class="text-sm uppercase text-gray-700 font-bold">{post.metadata.createdAt.split('T')[0]}</p>
         <h2 class="text-2xl mb-2 font-bold">{post.metadata.title}</h2>
         <h3 class="text-lg text-gray-700">{post.metadata.summary}</h3>
       </div>
--- services/website/src/routes/blog/_layout.svelte
+++ services/website/src/routes/blog/_layout.svelte
@@ -18,36 +18,50 @@
 		</div>
 
 		<div class="px-8 w-56 flex-shrink-0" style="min-width: 10rem">
-			<h3 class="text-2xl">Category:</h3>
-			<ul class="mb-20">
-				{#each categories as t}
-					<li class="leading-none mb-4">
-						<a href="/blog/category/{t.slug}">
-							<div class="pb-1">{t.name}</div>
-							<div class="text-sm text-gray-600">{t.name_en}</div>
-						</a>
-					</li>
-				{/each}
-			</ul>
+			<div class="widget">
+				<h3 class="text-2xl">Category:</h3>
+				<ul class="mb-20">
+					{#each categories as t}
+						<li class="leading-none mb-4">
+							<a href="/blog/category/{t.slug}">
+								<div class="pb-1">{t.name}</div>
+								<div class="text-sm text-gray-600">{t.name_en}</div>
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
 
-			<h3 class="text-2xl">Tag:</h3>
-			<ul class="mb-20">
-				{#each tags as t}
-					<li><a href="/blog/tags/{t}">{t}</a></li>
-				{/each}
-			</ul>
+			<div class="widget">
+				<h3 class="text-2xl">Tag:</h3>
+				<ul class="mb-20">
+					{#each tags as t}
+						<li><a href="/blog/tags/{t}">{t}</a></li>
+					{/each}
+				</ul>
+			</div>
 
-			<h3 class="text-2xl">Archive:</h3>
-			<ul class="mb-20">
-				<li>..</li>
-			</ul>
+			<div class="widget">
+				<h3 class="text-2xl">Archive:</h3>
+				<ul class="mb-20">
+					<li>..</li>
+				</ul>
+			</div>
 
-			<h3 class="text-2xl">Author:</h3>
-			<ul class="mb-20">
-				{#each authors as t}
-					<li><a href="/blog/author/{t.slug}">{t.name}</a></li>
-				{/each}
-			</ul>
+			<div class="widget">
+				<h3 class="text-2xl">Author:</h3>
+				<ul class="mb-20">
+					{#each authors as t}
+						<li><a href="/blog/author/{t.slug}">{t.name}</a></li>
+					{/each}
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
+
+<style>
+	.widget {
+		@apply px-4 py-8 shadow rounded mb-4;
+	}
+</style>
