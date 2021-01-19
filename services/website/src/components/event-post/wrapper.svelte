f5a571971d4f3577e711175760e7304b7d874d7e
cliff <cliff.wong@ehlacademy.org>
2021-01-19 18:51:52 +0800

    register button

--- services/website/src/components/event-post/wrapper.svelte
+++ services/website/src/components/event-post/wrapper.svelte
@@ -6,6 +6,7 @@
   import SeoHeadPost from "../seo/head-post.svelte";
 
   export let post;
+  let show_reg_info
 </script>
 
 <style>
@@ -19,12 +20,12 @@
   <link href="prism.css" rel="stylesheet" />
 </svelte:head>
 
-<div class="container max-w-6xl mx-auto px-4 mb-20">
+<div class="container max-w-6xl mx-auto px-0 mb-8 sm:px-4 sm:pb-4">
   <img
           class="mx-auto w-3/4 max-w-lg rounded-lg"
           src="event-posts/{post.metadata.createdAt.split('T')[0]}-{post.metadata.slug}/cover.jpg" alt="cover"/>
 
-  <div class="bg-white p-8 pt-16 shadow rounded -mt-8 container mx-auto">
+  <div class="bg-white p-4 sm:p-8 pt-16 shadow rounded -mt-8 container mx-auto">
     <div class="text-blue-700 my-8">
       <div>日期：{post.metadata.event_start_date}</div>
       {#if post.metadata.event_time}
@@ -35,6 +36,30 @@
       {/if}
       {#if post.metadata.event_fee}
         <div>費用：{post.metadata.event_fee}</div>
+      {/if}
+      <div class="mt-4">
+        <button class="px-6 flex h-10 min-w-40 items-center justify-center bg-blue-500 hover:bg-blue-700 text-white rounded" on:click={() => {show_reg_info = true}}>
+          🙋 報名 🙋‍
+        </button>
+      </div>
+
+      {#if show_reg_info}
+        <div transition:fade
+             class="fixed inset-0 flex items-center p-4"
+             on:click={() => {show_reg_info = false}}
+             style="background: rgba(0,0,0,0.3)">
+          <div class="mx-auto bg-white rounded-lg max-w-2xl p-4">
+            <h2 class="text-lg font-bold mb-4">可以用電郵或Facebook PM我地報名:</h2>
+            <div class="flex justify-center">
+              <a href="https://www.facebook.com/empower.hk/" target="_blank">
+                <img src="images/facebook.svg" alt="facebook" width="60">
+              </a>
+              <a href="mailto:info@empower.hk" class="ml-4">
+                <img src="images/email.svg" alt="email" width="60">
+              </a>
+            </div>
+          </div>
+        </div>
       {/if}
     </div>
 
--- services/website/src/components/footer.svelte
+++ services/website/src/components/footer.svelte
@@ -1,8 +1,8 @@
-<footer class="bg-gray-200 pt-8">
+<footer class="bg-gray-200 pt-8 px-4">
   <div class="pb-8 text-center">
     <img src="images/logo.png" alt="logo" class="mx-auto w-16 mb-4">
     <p class="mb-4"><b>Empower</b> 由一群懷抱夢想與理想，追求不平凡人生的平凡人組成。</p>
     <p class="font-sm mb-4 text-gray-700">Email: info@empower.hk</p>
-    <p class="text-sm text-gray-700">Empower © Copyright 2018. All Rights Reserved</p>
+    <p class="text-sm text-gray-700">Empower © Copyright {new Date().getFullYear()}. All Rights Reserved</p>
   </div>
 </footer>
