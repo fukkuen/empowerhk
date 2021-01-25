8aaabfaa4d9720ad002a6c8b950712cb3f3cdb79
cliff <cliff.wong@ehlacademy.org>
2021-01-25 12:22:50 +0800

    pagination

--- services/website/src/routes/blog/index.json.js
+++ services/website/src/routes/blog/index.json.js
@@ -1,11 +1,22 @@
 import posts from "./_posts.js";
 
 export const get = (req, res) => {
+  console.log('cliff: ', req.query.page)
+  const page = Number(req.query.page) || 0
+  const size = 9
+  const start = page * size
+  const end = start + size
+  const filtered_post = posts.slice(start, end)
+  const total = posts.length
+  console.log(start, end, filtered_post.length)
+
   res.writeHead(200, {
     "Content-Type": "application/json",
   });
   const result = {
-    posts
+    posts: filtered_post,
+    page: page,
+    is_last: end >= total
   }
   res.end(JSON.stringify(result));
 };
--- services/website/src/routes/blog/index.svelte
+++ services/website/src/routes/blog/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
 	export function preload({ params, query }) {
-		return this.fetch(`blog.json`)
+		return this.fetch(`blog.json?page=1`)
 				.then(r => r.json());
 	}
 </script>
@@ -8,12 +8,42 @@
 <script>
 	import Preview from '../../components/blog-post/previews/index.svelte'
 
+	export let page
 	export let posts
+	export let is_last
 
 	const getCover = (post) => {
 		return `blog-posts/${
 				post.metadata.createdAt.split("T")[0]
 		}-${post.metadata.slug}/cover-preview.jpg`
+	}
+
+	const loadMore = async () => {
+		page = 1 + Number(page)
+		const res = await fetch('blog.json?page=' + page)
+		const data = await res.json()
+		const new_posts = data.posts
+		is_last = data.is_last
+		console.log('new_posts', new_posts)
+		posts = [...posts, ...new_posts]
+	}
+
+	const loadMoreHandler = node => {
+		const observer = new IntersectionObserver((entries, observer) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					loadMore()
+				}
+			});
+		});
+
+		observer.observe(node);
+
+		return {
+			destroy() {
+				observer.disconnect();
+			}
+		};
 	}
 </script>
 
@@ -21,5 +51,9 @@
 {#each posts as post}
 	<Preview {post}/>
 {/each}
+
+{#if !is_last}
+	<div use:loadMoreHandler class="text-center my-4">更多...</div>
+{/if}
 
 
