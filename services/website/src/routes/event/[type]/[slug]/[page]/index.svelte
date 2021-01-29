7fb51c1443d1e33ead7303c5a17a8b98cafd1e21
cliff <cliff.wong@ehlacademy.org>
2021-01-29 19:27:13 +0800

    event

---
+++ services/website/src/routes/event/[type]/[slug]/[page]/index.svelte
@@ -0,0 +1,52 @@
+<script context="module">
+	export function preload({ params }) {
+		const {type, slug, page} = params
+		return this.fetch(`event/${type}/${slug}/${page}.json`).then(r => r.json());
+	}
+</script>
+
+<script>
+	import Preview from '../../../../../components/event-post/previews/index.svelte'
+	import loadMoreHandler from "../../../../../helpers/load-more-handler";
+	import { stores } from '@sapper/app';
+
+	export let posts
+	export let is_last
+	let page_number
+	const { page } = stores();
+
+	let is_render = true
+	const saved_param = $page.params
+	$: {
+		if (saved_param !== $page.params) {
+			is_render = false
+			setTimeout(() => {
+				is_render = true
+			}, 10)
+		}
+	}
+
+	$: type = $page.params.type
+	$: slug = $page.params.slug
+
+	const loadMore = async () => {
+		page_number = Number(page_number) + 1
+		const res = await fetch(`blog/${type}/${slug}/${page_number}.json`)
+		const data = await res.json()
+		const new_posts = data.posts
+		is_last = data.is_last
+		posts = [...posts, ...new_posts]
+	}
+</script>
+
+{#if is_render}
+<div class="mx-auto px-4 max-w-screen-lg grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+	{#each posts as post}
+		<Preview {post}/>
+	{/each}
+</div>
+{/if}
+
+{#if !is_last}
+	<div use:loadMoreHandler={loadMore} class="text-center my-4">更多...</div>
+{/if}
--- services/website/src/routes/event/index.svelte
+++ services/website/src/routes/event/index.svelte
@@ -1,40 +1,5 @@
 <script context="module">
-	export function preload({ params, query }) {
-		return this.fetch(`event.json`)
-				.then(r => r.json());
+	export function preload() {
+		return this.redirect(302, 'event/category/all/1')
 	}
 </script>
-
-<script>
-	import Preview from '../../components/event-post/previews/index.svelte'
-	import loadMoreHandler from "../../helpers/load-more-handler";
-
-	export let posts
-	export let page
-	export let is_last
-
-	const getCover = (post) => {
-		return `event-posts/${
-				post.metadata.createdAt.split("T")[0]
-		}-${post.metadata.slug}/cover-preview.jpg`
-	}
-
-	const loadMore = async () => {
-		page = 1 + Number(page)
-		const res = await fetch('event.json?page=' + page)
-		const data = await res.json()
-		const new_posts = data.posts
-		is_last = data.is_last
-		posts = [...posts, ...new_posts]
-	}
-</script>
-
-<div class="mx-auto px-4 max-w-screen-lg grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-	{#each posts as post}
-		<Preview {post}/>
-	{/each}
-</div>
-
-{#if !is_last}
-	<div use:loadMoreHandler={loadMore} class="text-center my-4">更多...</div>
-{/if}
