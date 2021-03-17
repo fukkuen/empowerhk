a0b4239a56ccef9c0035fc9e8b214c54ef31733b
fukkuen <fukkuen@gmail.com>
2021-03-17 13:38:57 +0000

    changes

---
+++ services/website/src/routes/blog/nutrition-210316/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2021-03-16-nutrition-210316/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
