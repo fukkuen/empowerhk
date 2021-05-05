e5bea180df2f6a38fadf15bf074864a9c364412a
fukkuen <fukkuen@gmail.com>
2021-05-05 02:08:42 +0000

    add event

---
+++ services/website/src/routes/event/keepfit-202105/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2021-05-10-keepfit-202105/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
