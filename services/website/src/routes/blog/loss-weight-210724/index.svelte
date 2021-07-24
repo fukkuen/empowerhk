27e5ab358de540883010ec66a158f21107c5af56
fukkuen <fukkuen@gmail.com>
2021-07-24 15:25:52 +0000

    added posts and events

---
+++ services/website/src/routes/blog/loss-weight-210724/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2021-07-24-loss-weight-210724/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
