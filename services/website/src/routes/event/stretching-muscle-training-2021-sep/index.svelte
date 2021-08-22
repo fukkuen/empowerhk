b8a10081989d033f86303cf6979ad71656bccf50
fukkuen <fukkuen@gmail.com>
2021-08-22 12:11:49 +0000

    added posts

---
+++ services/website/src/routes/event/stretching-muscle-training-2021-sep/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2021-09-22-stretching-muscle-training-2021-sep/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
