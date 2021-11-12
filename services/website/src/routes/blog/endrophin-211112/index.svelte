4d37805fc5ca877e7c46d05d6d8b8e4101c218f2
fukkuen <fukkuen@gmail.com>
2021-11-12 13:24:02 +0000

    added post

---
+++ services/website/src/routes/blog/endrophin-211112/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2021-11-12-endrophin-211112/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
