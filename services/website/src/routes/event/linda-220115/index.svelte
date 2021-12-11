d96e7c7020ac3aff9f45ab5184d804210cd82b85
fukkuen <fukkuen@gmail.com>
2021-12-11 03:39:17 +0000

    added and delete

---
+++ services/website/src/routes/event/linda-220115/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2022-01-15-linda-220115/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
