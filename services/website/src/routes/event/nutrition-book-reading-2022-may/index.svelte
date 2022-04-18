7332d329307a1b9c01a6c4d3c50f75776f4b0fbe
fukkuen <fukkuen@gmail.com>
2022-04-18 14:26:01 +0000

    added

---
+++ services/website/src/routes/event/nutrition-book-reading-2022-may/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2022-04-05-nutrition-book-reading-2022-may/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
