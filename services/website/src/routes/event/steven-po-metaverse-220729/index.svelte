160b87dcc33e65257b54fe9cd6ba49bf9bdc280f
fukkuen <fukkuen@gmail.com>
2022-07-06 14:28:05 +0000

    added

---
+++ services/website/src/routes/event/steven-po-metaverse-220729/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/event-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../event-posts/2022-07-29-steven-po-metaverse-220729/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
