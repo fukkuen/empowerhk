3ba9539e4287c930d8b0662d13f933040677c616
fukkuen <fukkuen@gmail.com>
2022-07-06 14:53:31 +0000

    amend

---
+++ services/website/src/routes/blog/backache-220508/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2022-05-08-backache-220508/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
